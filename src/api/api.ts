5dc67569e2fd0135465dc48ffc6ad63a0234d26e
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-12-14 20:13:05 +0300

    fix/TP-54: headers were added

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,30 +3,30 @@ import { ServerDataForPUTRequest, ServerDataType } from '../redux/actions/formAc
 import { ServerResponse, socialsIcons } from '../components/UserDataFormContainer/UserDataForm/UserDataFormTypes';
 
 const api = axios.create({
-  baseURL: 'https://example.com/api',
+  baseURL: 'http://localhost:8081/',
 });
 
 export const sendFormData = async (formData: ServerDataType): Promise<ServerResponse> => {
-  const response = await api.post<ServerResponse>('/users', formData);
+  const response = await api.post<ServerResponse>('users', formData);
   return response.data;
 };
 
 export const getSocialsData = async (): Promise<socialsIcons[]> => {
-  const response = await api.get<socialsIcons[]>('/social');
+  const response = await api.get<socialsIcons[]>('social');
   return response.data;
 };
 
 export const getUserData = async (userId: string) => {
-  const response = await api.get(`/users/${userId}`);
+  const response = await api.get(`users/${userId}`);
   return response.data;
 };
 
 export const checkEditCode = async (userId: string, editCode: string): Promise<{ success: boolean }> => {
-  const response = await api.post('/users/edit-check', { user_id: userId, edit_code: editCode });
+  const response = await api.post('users/edit-check', { user_id: userId, edit_code: editCode }, { headers: { 'Content-Type': 'application/json' } });
   return response.data;
 };
 
 export const updateUserData = async (userId: number, updatedData: ServerDataForPUTRequest) => {
-  const response = await api.put(`/users/${userId}`, updatedData);
+  const response = await api.put(`users/${userId}`, updatedData, { headers: { 'Content-Type': 'application/json' } });
   return response.data;
 };
